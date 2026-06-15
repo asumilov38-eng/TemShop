50123012414069de402d2662e7a7c422722c1695
Andrey Shumilov <asumilov38@gmail.com>
2026-06-15 22:38:54 +0300

    Добавлен новый товар: Новые крутые часы

--- data.js
+++ data.js
@@ -6,6 +6,6 @@ const products = [
         description: "Классические солнцезащитные очки с поляризацией, идеальная защита от солнца и стильный вид, подходит для мужчин. Продажа при личной встрече.",
         price: 400,
         oldPrice: 650, // Если скидки нет, просто удали эту строку или поставь null
-        images: ["images/DSC_0012.JPG", "images/DSC_0013.JPG", "images/DSC_0014.JPG", "images/DSC_0015.JPG"] // Массив фоток. Можно 1, можно 5.
+        images: ["images/Group 3.jpg", "images/Group 4.jpg", "images/Group 2.jpg", "images/Group 1.jpg"] // Массив фоток. Можно 1, можно 5.
     }
 ];
